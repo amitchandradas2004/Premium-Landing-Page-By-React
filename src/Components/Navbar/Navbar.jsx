32eac4165c748e2445d408267dddadb0987d2091
amitchandradas2004 <amitcdas792@gmail.com>
2026-04-27 23:57:53 +0600

    Navbar is updated

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,13 +2,15 @@ import { FaAngleRight } from "react-icons/fa";
 import Logo from "../../assets/Vector - 0.png";
 const Navbar = () => {
   return (
-    <nav className="bg-linear-to-r from-black to-gray-800 py-2 text-white">
+    <nav className="bg-linear-to-r from-[#10113c] via-[#0F0C17]  to-[#290e2f] px-2 md:px-0 py-2 text-white">
       <div className="container mx-auto flex justify-between items-center py-2 border border-gray-600 rounded-2xl px-2">
         <div className="flex items-center gap-2">
           <img src={Logo} />
-          <h2 className="text-3xl lg:text-4xl source font-bold">IdentityHub</h2>
+          <h2 className="text-xl md:text-3xl lg:text-4xl source font-bold">
+            IdentityHub
+          </h2>
         </div>
-        <div className="abeezee flex items-center gap-3 opacity-80 text-xs">
+        <div className="abeezee flex md:inline-flex hidden items-center gap-3 opacity-80 text-xs">
           <p>Services</p>
           <p>Features</p>
           <p>Pricing</p>
@@ -16,7 +18,7 @@ const Navbar = () => {
           <p>About us</p>
         </div>
         <div>
-          <button className="flex items-center border border-[#8D1EA2] rounded-2xl p-2 text-center">
+          <button className="flex items-center border border-[#8D1EA2] rounded-xl p-2 text-center text-xs md:text-xl">
             Start Free Trial{" "}
             <span>
               <FaAngleRight />
