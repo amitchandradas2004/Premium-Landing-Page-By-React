1dd7d2a6e67954c5cc5b0b7a1600c404ec863fdd
amitchandradas2004 <amitcdas792@gmail.com>
2026-05-05 23:23:55 +0600

    Secure All Data is added

--- src/Components/Feature/Feature.jsx
+++ src/Components/Feature/Feature.jsx
@@ -1,13 +1,13 @@
 import Check1 from "../../assets/check1.png";
 import Check2 from "../../assets/check2.png";
-// import Check3 from "../../assets/check3.png";
+import Check3 from "../../assets/check3.png";
 import Image1 from "../../assets/image1.png";
 import Image2 from "../../assets/image2.png";
-// import Image3 from "../../assets/image3.png";
+import Image3 from "../../assets/image3.png";
 const Feature = () => {
   return (
     <div className="bg-black">
-      <div className="container mx-auto">
+      <div className="container mx-auto px-2 md:px-0">
         {/* glowing */}
         <div className="relative  text-white items-center justify-center overflow-hidden py-10 container mx-auto">
           {/* <!-- Glow layer --> */}
@@ -32,9 +32,9 @@ const Feature = () => {
             </p>
           </div>
           <div className="py-10">
-            <div className="1 flex flex-col md:flex-row justify-between items-center gap-5">
+            <div className="1 flex flex-col md:flex-row justify-between items-center gap-5 bg-gray-900 rounded-3xl  py-10 px-5">
               <div className="left w-full md:w-2/3">
-                <h3 className="text-[#CAD1E9] text-4xl font-bold">
+                <h3 className="text-[#1E21E9] text-4xl font-bold">
                   Protect Personal information
                 </h3>
                 <div className="space-y-3 mt-3">
@@ -73,9 +73,9 @@ const Feature = () => {
                 <img src={Image1} className="" />
               </div>
             </div>
-            <div className="1 flex flex-col md:flex-row justify-between items-center gap-5 py-10">
+            <div className="1 flex flex-col md:flex-row justify-between items-center gap-5 my-10 bg-gray-900 rounded-3xl py-10 px-5">
               <div className="left w-full md:w-2/3">
-                <h3 className="text-[#CAD1E9] text-4xl font-bold">
+                <h3 className="text-[#7950FF] text-4xl font-bold">
                   Detailed Reporting
                 </h3>
                 <div className="space-y-3 mt-3">
@@ -112,6 +112,48 @@ const Feature = () => {
               </div>
               <div className="right">
                 <img src={Image2} className="" />
+              </div>
+            </div>
+
+            <div className="1 flex flex-col md:flex-row justify-between items-center gap-5 my-10 bg-gray-900 rounded-3xl py-10 px-5">
+              <div className="left w-full md:w-2/3">
+                <h3 className="text-[#098709] text-4xl font-bold">
+                  Secure All Data
+                </h3>
+                <div className="space-y-3 mt-3">
+                  <div className="flex items-center AbeeZee gap-3">
+                    <img src={Check3} className="w-5" />
+                    <p className="text-[#CAC6DD] text-xl">
+                      The first step involves identifying all places where your
+                      personal information might be present.
+                    </p>
+                  </div>
+                  <div className="flex items-center AbeeZee gap-3">
+                    <img src={Check3} className="w-5" />
+                    <p className="text-[#CAC6DD] text-xl">
+                      Once you've identified where your information is held, the
+                      next step is to contact the administrators of this
+                      platform
+                    </p>
+                  </div>
+                  <div className="flex items-center AbeeZee gap-3">
+                    <img src={Check3} className="w-5" />
+                    <p className="text-[#CAC6DD] text-xl">
+                      Removing personal information is not a one-time task but
+                      requires ongoing vigilance.
+                    </p>
+                  </div>
+                  <div className="flex items-center AbeeZee gap-3">
+                    <img src={Check3} className="w-5" />
+                    <p className="text-[#CAC6DD] text-xl">
+                      There are professional services and tools available that
+                      specialise in personal information removal.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="right">
+                <img src={Image3} className="" />
               </div>
             </div>
           </div>
